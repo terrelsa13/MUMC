78270b689be99edea2cfed691abd53b33c4ea9ee
terrelsa13 <terrelsa13@gmail.com>
2022-02-14 21:09:33 -0500

    Minor Rev Bump 1.2.2 to 1.2.3
    Minor Rev Bump 1.2.2 to 1.2.3

--- media_cleaner.py
+++ media_cleaner.py
@@ -673,7 +673,7 @@ def get_auth_key(server_url, username, password, server_brand):
     #else:
         #xAuth = 'X-Jellyfin-Authorization'
 
-    headers = {'X-Emby-Authorization' : 'Emby UserId="' + username  + '", Client="media_cleaner.py", Device="Multi-User Media Cleaner", DeviceId="MUMC", Version="1.2.2", Token=""', 'Content-Type' : 'application/json'}
+    headers = {'X-Emby-Authorization' : 'Emby UserId="' + username  + '", Client="media_cleaner.py", Device="Multi-User Media Cleaner", DeviceId="MUMC", Version="1.2.3", Token=""', 'Content-Type' : 'application/json'}
 
     req = request.Request(url=server_url + '/Users/AuthenticateByName', data=DATA, method='POST', headers=headers)
 
