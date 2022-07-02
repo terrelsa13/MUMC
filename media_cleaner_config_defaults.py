6a4366ecb13240ad791d760d65a1ab427b84c972
terrelsa13 <terrelsa13@gmail.com>
2022-07-02 00:23:41 -0400

    Update default config file with new config options and remove obsolete config options

--- media_cleaner_config_defaults.py
+++ media_cleaner_config_defaults.py
@@ -14,10 +14,30 @@ def get_default_config_values(config_value):
         #'example_string_config':'abc'
         #'example_boolean_config':True
 
-        'played_age_movie':-1,
-        'played_age_episode':-1,
-        'played_age_audio':-1,
-        'played_age_audiobook':-1,
+        'movie_condition':'played',
+        'episode_condition':'played',
+        'audio_condition':'played',
+        'audiobook_condition':'played',
+
+        'movie_condition_days':-1,
+        'episode_condition_days':-1,
+        'audio_condition_days':-1,
+        'audiobook_condition_days':-1,
+
+        'movie_play_count_comparison':'>=',
+        'episode_play_count_comparison':'>=',
+        'audio_play_count_comparison':'>=',
+        'audiobook_play_count_comparison':'>=',
+
+        'movie_play_count':1,
+        'episode_play_count':1,
+        'audio_play_count':1,
+        'audiobook_play_count':1,
+
+        'multiuser_play_count_movie':0,
+        'multiuser_play_count_episode':0,
+        'multiuser_play_count_audio':0,
+        'multiuser_play_count_audiobook':0,
 
         'keep_favorites_movie':1,
         'keep_favorites_episode':1,
@@ -39,16 +59,6 @@ def get_default_config_values(config_value):
         'whitetag':'',
 
         'minimum_number_episodes':0,
-
-        'played_count_movie_action':0,
-        'played_count_episode_action':0,
-        'played_count_audio_action':0,
-        'played_count_audiobook_action':0,
-
-        'played_count_movie':1,
-        'played_count_episode':1,
-        'played_count_audio':1,
-        'played_count_audiobook':1,
 
         'keep_favorites_advanced_movie_genre':0,
         'keep_favorites_advanced_movie_library_genre':0,
