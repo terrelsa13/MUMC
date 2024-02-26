37c506a500c644b625b2dec56ebeb6e877aa0710
terrelsa13 <terrelsa13@gmail.com>
2024-02-25 19:58:39 -0500

    v5 Fix issue with comparing libraries

--- mumc_modules/mumc_compare_items.py
+++ mumc_modules/mumc_compare_items.py
@@ -55,32 +55,51 @@ def does_index_exist(item, indexvalue, the_dict):
 
 #Determine if there is a matching item or Determine if an item starts with the other
 def get_isItemMatching_doesItemStartWith(item_one,item_two,the_dict):
-    #for Ids in Microsoft Windows, replace backslashes in Ids with forward slash
-    item_one = item_one.replace('\\','/')
-    item_two = item_two.replace('\\','/')
 
-    #read and split Ids to compare to
-    item_one_split=item_one.split(',')
-    item_two_split=item_two.split(',')
+    #for Ids in Microsoft Windows, replace backslashes in Ids with forward slash
+    if (isinstance(item_one,list)):
+        for item1 in item_one:
+            if (not (item1 == None)):
+                itempos = item_one.index(item1)
+                item_one[itempos] = item1.replace('\\','/')
+    elif (isinstance(item_one,str)):
+        item_one = item_one.replace('\\','/')
+        item_one_list=[]
+        item_one_list.append(item_one)
+        item_one=item_one_list
+
+    #for Ids in Microsoft Windows, replace backslashes in Ids with forward slash
+    if (isinstance(item_two,list)):
+        for item2 in item_two:
+            if (not (item2 == None)):
+                itempos = item_two.index(item2)
+                item_two[itempos] = item2.replace('\\','/')
+    elif (isinstance(item_two,str)):
+        item_two = item_two.replace('\\','/')
+        item_two_list=[]
+        item_two_list.append(item_two)
+        item_two=item_two_list
 
     #DEBUG log formatting
     if (the_dict['DEBUG']):
         appendTo_DEBUG_log('\n',1,the_dict)
 
     #determine if media Id matches one of the other Ids
-    for single_item_one in item_one_split:
-            for single_item_two in item_two_split:
+    for single_item_one in item_one:
+            for single_item_two in item_two:
                 if (the_dict['DEBUG']):
                     appendTo_DEBUG_log('\nComparing the below two items',3,the_dict)
                     appendTo_DEBUG_log('\n\'' + str(single_item_one) + '\'' + ':' + '\'' + str(single_item_two) + '\'',3,the_dict)
 
                 if (single_item_one and single_item_two):
                     if (single_item_one == single_item_two):
+                        #found a full match; return true and the matching value
                         return True,single_item_one
                     elif (single_item_one.startswith(single_item_two)):
+                        #found a partial match; return true and the matching value
                         return True,single_item_two
                     elif (single_item_two.startswith(single_item_one)):
-                        #found a match; return true and the matching value
+                        #found a partial match; return true and the matching value
                         return True,single_item_one
 
     #nothing matched; return false and empty string
