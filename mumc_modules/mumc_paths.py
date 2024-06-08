06c04aca27f1f0f563ecf9869b276836032f2c40
terrelsa13 <terrelsa13@gmail.com>
2024-06-08 00:23:55 -0400

    v5 Fix path releated typeError

--- mumc_modules/mumc_paths.py
+++ mumc_modules/mumc_paths.py
@@ -40,7 +40,7 @@ def getFullPathName(filePathName):
         fullPathName=str(Path(filePathName).resolve())
     else:
         for directory in reversed(path):
-            if (doesFileExist(directory + '/' + filePathName)):
+            if (doesFileExist(directory / filePathName)):
                 fullPathName=str(Path(directory / filePathName))
     return fullPathName
 
