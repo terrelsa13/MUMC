fd5d33ac0ea6fd2b5d0155e3db679dc9a1eedf27
terrelsa13 <terrelsa13@gmail.com>
2024-09-02 11:06:29 -0400

    v5.8.x Fix bug with [Emby library subfolders](https://github.com/terrelsa13/MUMC/wiki/Known-Limitations#emby-library-sub-folder-limitations)

--- mumc_modules/mumc_get_media.py
+++ mumc_modules/mumc_get_media.py
@@ -824,7 +824,7 @@ def init_getMedia(the_dict):
                 parent_id='lib_id'
             else:
                 if (('subfolder_id' in lib_info) and (not (lib_info['subfolder_id'] == None))):
-                    library_id='subfolder_id'
+                    library_id='lib_id'
                     parent_id='lib_id'
                 else:
                     library_id='lib_id'
@@ -841,7 +841,7 @@ def init_getMedia(the_dict):
                 parent_id='lib_id'
             else:
                 if (('subfolder_id' in lib_info) and (not (lib_info['subfolder_id'] == None))):
-                    library_id='subfolder_id'
+                    library_id='lib_id'
                     parent_id='lib_id'
                 else:
                     library_id='lib_id'
