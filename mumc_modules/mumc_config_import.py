3e1dbf1ab7872dad3df9bbd8489fb87972e99dc1
terrelsa13 <terrelsa13@gmail.com>
2025-04-17 22:30:27 -0400

    v5.11.x Docker

--- mumc_modules/mumc_config_import.py
+++ mumc_modules/mumc_config_import.py
@@ -29,6 +29,8 @@ def cannotFindConfig(init_dict,cmdopt_dict):
             init_dict['mumc_path']=cmdopt_dict['altConfigPath']
             init_dict['config_file_name_yaml']=cmdopt_dict['altConfigFileExt']
         build_configuration_file(init_dict)
+        while (True):
+            time.sleep(60)
         #exit gracefully
         sys.exit(0)
 
