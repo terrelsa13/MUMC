20bf35b77ea8299b07e58b8f3980be73783b433c
terrelsa13 <terrelsa13@gmail.com>
2025-07-17 20:59:18 -0400

    v5.11.33 Handle PUT binary data return Stop overwritting variable before finishing using it

--- mumc_modules/mumc_url.py
+++ mumc_modules/mumc_url.py
@@ -232,8 +232,8 @@ def requestURL(the_dict, url='', debugState=0, requestDebugMessage='message unde
                 if ((response.getcode() == 200) or (response.getcode() == 202)):
                     try:
                         source = response.read()
-                        data = json.loads(source)
-                        if ((url.method == 'GET')):
+                        if ((url.method == 'GET') or (url.method == 'PUT') or (url.method == 'POST')):
+                            data = json.loads(source)
                             the_dict['cached_data'].addEntryToCache(url.full_url,data)
                         elif (url.method == 'DELETE'):
                             data = response.getcode()
