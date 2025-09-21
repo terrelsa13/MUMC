741f6ccbf083a9762091146e323a2863991260ab
terrelsa13 <terrelsa13@gmail.com>
2025-09-21 18:41:59 -0400

    Update mumc_versions.py
    v5.12.0-alpha

--- mumc_modules/mumc_versions.py
+++ mumc_modules/mumc_versions.py
@@ -4,7 +4,7 @@ from mumc_modules.mumc_url import requestURL,build_emby_jellyfin_request_message
 
 #Get the current MUMC version
 def get_MUMC_version():
-    return '5.11.36'
+    return '5.12.0-alpha'
 
 
 #Get the min config version
